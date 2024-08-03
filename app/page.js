77f7421e3cc91840784dda3468426a94aa298df6
swathi-k <swathikughan@gmail.com>
2024-08-03 18:53:36 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,43 +1,37 @@
-// pages/about.js
-
+import dynamic from 'next/dynamic';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from '../components/Navbar';
-import Header from '../components/Header'; 
-import Partner from '../components/Partner';
-import About from '../components/About';
-import Features from '../components/Features';
-import Benefits from '../components/Benefits';
-import Testimonials from '../components/Testimonials';
-import CaseStudies from '../components/CaseStudies';
-import Pricing from '../components/Pricing';
-import Faq from '../components/Faq';
-import ContactUs from '../components/ContactUs';
-import Articles from '../components/Articles';
 
-
-
-
+// Dynamically import components that may require browser-specific APIs
+const Navbar = dynamic(() => import('../components/Navbar'), { ssr: false });
+const Header = dynamic(() => import('../components/Header'), { ssr: false });
+const Partner = dynamic(() => import('../components/Partner'), { ssr: false });
+const About = dynamic(() => import('../components/About'), { ssr: false });
+const Features = dynamic(() => import('../components/Features'), { ssr: false });
+const Benefits = dynamic(() => import('../components/Benefits'), { ssr: false });
+const Testimonials = dynamic(() => import('../components/Testimonials'), { ssr: false });
+const CaseStudies = dynamic(() => import('../components/CaseStudies'), { ssr: false });
+const Pricing = dynamic(() => import('../components/Pricing'), { ssr: false });
+const Faq = dynamic(() => import('../components/Faq'), { ssr: false });
+const ContactUs = dynamic(() => import('../components/ContactUs'), { ssr: false });
+const Articles = dynamic(() => import('../components/Articles'), { ssr: false });
 
 const Tanus = () => {
     return (
         <div>
-            
-            < Navbar />
-            < Header />
-            < Partner />
-            < About />
-            < Features />
-            < Benefits />
-            < Testimonials />
-            < CaseStudies />
-            < Pricing />
-            < Faq />
-            < Articles />
-            < ContactUs />
-           
-           
+            <Navbar />
+            <Header />
+            <Partner />
+            <About />
+            <Features />
+            <Benefits />
+            <Testimonials />
+            <CaseStudies />
+            <Pricing />
+            <Faq />
+            <Articles />
+            <ContactUs />
         </div>
-        
     );
 }
+
 export default Tanus;
